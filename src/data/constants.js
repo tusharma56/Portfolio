89ab62ef75e0b438c5f912bf32cbeece7bb4085d
Tushar Sharma <jitendrakumarsharma269@gmail.com>
2024-11-15 03:07:10 +0530

    first commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -6,7 +6,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   resume:
-  "https://drive.google.com/file/d/16ENYnIFK0XpT8KZ136DnQjT4hao8RpDQ/view?usp=drive_link",
+  "https://drive.google.com/file/d/1O8a0ySVSitCfamumiS1FLH02uvefQ6yq/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/tushar-sharma-167a8b236/",
   insta: "https://www.instagram.com/https.tushar_sharma/",
 };
